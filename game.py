5afd73a368a398df70b7c62bc55a28eb0c79494d
TheAndromeda <anvimitra@gmail.com>
2026-07-17 21:31:01 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+import pgzrun
+import random
+
+WIDTH = 800
+HEIGHT = 600
+TITLE = "Waste Management"
+CENTER_X = 400
+CENTER_Y = 300
+CENTER = (CENTER_X,CENTER_Y)
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS = [""]
