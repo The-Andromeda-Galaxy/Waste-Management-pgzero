64216ecb05a2403375b22a4c465548fb788e5235
TheAndromeda <anvimitra@gmail.com>
2026-07-31 21:30:21 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -12,18 +12,29 @@ START_SPEED = 10
 ITEMS = ["battery","bottle","chips","glass","plastic"]
 
 is_game_over = False
-Is_game_complete = False
+is_game_complete = False
 current_level = 1
 
 items = []
 animations = []
 
 def draw():
+    global items,animations,current_level,is_game_complete,is_game_over
     screen.clear()
     screen.blit("bg",(0,0))
+    if is_game_over:
+        display_message("You Lose","Try again")
+        
 
 def update():
     pass
+
+def make_items(extra_items):
+    items_to_create = get_option(extra_items)
+    new_items = create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
 
 def get_option(extra_items):
     items_to_create = ["compost"]
@@ -47,7 +58,45 @@ def layout_items(items_to_layout):
         x_pos = (index+1)*gap_size
         item.x = x_pos
 
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center","bottom")
+        animation = animate(item,duration = duration,on_finished = game_over,y = HEIGHT)
+        animations.append(animation)
 
+def game_over():
+    global is_game_over
+    is_game_over = True
+
+def on_mouse_down(pos):
+    global items,current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "compost" in item.image:
+                game_complete()
+            else:
+                game_over()
+
+def game_complete():
+    global current_level,animations,is_game_complete,items
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        is_game_complete = True
+    else:
+        current_level += 1
+        animations = []
+        items = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running():
+            animation.stop()
+
+def display_message(main_text,sub_text):
+    screen.draw.text(main_text,fontsize = 60, center = CENTER, color = "#1D2910")
+    screen.draw.text(sub_text,fontsize = 40, center = (CENTER_X,CENTER_Y+50), color = "#141F09")
 
 
 
